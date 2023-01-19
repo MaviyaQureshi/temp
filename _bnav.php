8d242fbb111f1a4eeb04a678b8efa1a127f12ed6
Shrutii9 <112260757+Shrutii9@users.noreply.github.com>
2023-01-19 22:49:12 +0530

    Add files via upload

---
+++ _bnav.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <div class="logo">
+    <!--<a class="navbar-brand" href="/mylist">TO DO LIST</a>-->
+    <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/logo.png" alt="">
+        <span>Bazaar</span>
+      </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/rubix23/index.html">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/rubix23/login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/rubix23/signup.php">Signup</a>
+        </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="/rubix23/confirmation.php">Confirmation</a>
+        </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="/rubix23/browse.php">Browse</a>
+        </li>
+        
+      
+      <li class="nav-item">
+        <a class="nav-link" href="/rubix23/ContactUs.html">About Us</a>
+      </li>
+      </ul>
+    </div>
+  </div>
+</nav>
